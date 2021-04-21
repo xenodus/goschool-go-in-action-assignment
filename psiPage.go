439821696160d69e111d752c0790431c815b6f0c
alvin.yeoh <alvin.yeoh@gumi.sg>
2021-04-21 21:48:27 +0800

    error handling for psi

--- psiPage.go
+++ psiPage.go
@@ -10,39 +10,39 @@ import (
 
 func psiPage(res http.ResponseWriter, req *http.Request) {
 
-	api_url := "https://api.data.gov.sg/v1/environment/psi"
-
-	result, httpErr := http.Get(api_url)
-
-	if httpErr != nil {
-		fmt.Println("boo")
-	}
-
 	var psi = ""
 	var psiDescription = ""
 	var errorMsg = ""
 	var jsonResult map[string]interface{}
 
-	JSONData, _ := ioutil.ReadAll(result.Body)
-	marshalErr := json.Unmarshal(JSONData, &jsonResult)
-	tmp := jsonResult["items"].([]interface{})[0].(map[string]interface{})["readings"].(map[string]interface{})["psi_twenty_four_hourly"].(map[string]interface{})["national"]
+	api_url := "https://api.data.gov.sg/v1/environment/psi"
 
-	if marshalErr != nil || tmp == nil {
+	result, httpErr := http.Get(api_url)
+
+	if httpErr != nil {
 		errorMsg = "Error encountered fetching PSI"
 	} else {
-		psi = fmt.Sprintf("%v", tmp)
-		psiInt, err := strconv.Atoi(psi)
+		JSONData, _ := ioutil.ReadAll(result.Body)
+		marshalErr := json.Unmarshal(JSONData, &jsonResult)
+		tmp := jsonResult["items"].([]interface{})[0].(map[string]interface{})["readings"].(map[string]interface{})["psi_twenty_four_hourly"].(map[string]interface{})["national"]
 
-		if err == nil {
-			// https://www.haze.gov.sg/
-			if psiInt >= 0 && psiInt <= 55 {
-				psiDescription = "Normal"
-			} else if psiInt >= 56 && psiInt <= 150 {
-				psiDescription = "Elevated"
-			} else if psiInt >= 151 && psiInt <= 250 {
-				psiDescription = "High"
-			} else {
-				psiDescription = "Very High"
+		if marshalErr != nil || tmp == nil {
+			errorMsg = "Error encountered fetching PSI"
+		} else {
+			psi = fmt.Sprintf("%v", tmp)
+			psiInt, err := strconv.Atoi(psi)
+
+			if err == nil {
+				// https://www.haze.gov.sg/
+				if psiInt >= 0 && psiInt <= 55 {
+					psiDescription = "Normal"
+				} else if psiInt >= 56 && psiInt <= 150 {
+					psiDescription = "Elevated"
+				} else if psiInt >= 151 && psiInt <= 250 {
+					psiDescription = "High"
+				} else {
+					psiDescription = "Very High"
+				}
 			}
 		}
 	}
