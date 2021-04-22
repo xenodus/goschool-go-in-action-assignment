b20a6001401354344e4852a60daf493a9f593095
alvin.yeoh <alvin.yeoh@gumi.sg>
2021-04-22 16:48:14 +0800

    separated routes and build

--- config.go
+++ config.go
@@ -20,31 +20,3 @@ const strictNRIC = false
 const testFakeTime = false
 const testHour = 9
 const testMinute = 15
-
-// URLS
-const pageIndex = "/"
-const pageLogin = "/login"
-const pageLogout = "/logout"
-const pageRegister = "/register"
-const pageProfile = "/profile"
-const pageError = "/error"
-
-const pageMyAppointments = "/appointments"
-const pageNewAppointment = "/appointment/new"
-const pageEditAppointment = "/appointment/edit"
-
-const pageDoctors = "/doctors"
-
-const pageAdminDebug = "/admin/debug"
-const pageAdminAllAppointments = "/admin/appointments"
-const pageAdminEditAppointment = "/admin/appointment/edit"
-const pageAdminSessions = "/admin/sessions"
-const pageAdminUsers = "/admin/users"
-const pageAdminPaymentEnqueue = "/admin/payment/enqueue"
-
-const pageAdminPaymentDequeue = "/admin/payment/dequeue"
-const pageAdminPaymentDequeueToMissedQueue = "/admin/payment/dequeueToMissedQueue"
-const pageAdminPaymentDequeueToPaymentQueue = "/admin/payment/dequeueToPaymentQueue"
-
-const pagePaymentQueue = "/payment/queue"
-const pagePSI = "/psi"
---
+++ routes.go
@@ -0,0 +1,29 @@
+package main
+
+// URLS
+const pageIndex = "/"
+const pageLogin = "/login"
+const pageLogout = "/logout"
+const pageRegister = "/register"
+const pageProfile = "/profile"
+const pageError = "/error"
+
+const pageMyAppointments = "/appointments"
+const pageNewAppointment = "/appointment/new"
+const pageEditAppointment = "/appointment/edit"
+
+const pageDoctors = "/doctors"
+
+const pageAdminDebug = "/admin/debug"
+const pageAdminAllAppointments = "/admin/appointments"
+const pageAdminEditAppointment = "/admin/appointment/edit"
+const pageAdminSessions = "/admin/sessions"
+const pageAdminUsers = "/admin/users"
+const pageAdminPaymentEnqueue = "/admin/payment/enqueue"
+
+const pageAdminPaymentDequeue = "/admin/payment/dequeue"
+const pageAdminPaymentDequeueToMissedQueue = "/admin/payment/dequeueToMissedQueue"
+const pageAdminPaymentDequeueToPaymentQueue = "/admin/payment/dequeueToPaymentQueue"
+
+const pagePaymentQueue = "/payment/queue"
+const pagePSI = "/psi"
