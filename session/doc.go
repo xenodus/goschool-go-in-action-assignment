35585b7bd177fd31bc612edfdfbf010588a968fd
alvin.yeoh <alvin.yeoh@gumi.sg>
2021-05-03 16:36:28 +0800

    Docs

--- session/doc.go
+++ session/doc.go
@@ -1,3 +1,3 @@
-// Package session defines the session and notification types;
+// Package session defines the Session and Notification types;
 // It provides implementation for creating and deleting of server side session, client side cookie and use of notification inside session to pass messages between http request.
 package session
